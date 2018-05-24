98ee1909806c48302f3eb4eb4aa325e8974b28f1
Dyson1990 <Weaver1990@163.com>
2018-05-24 11:54:52 +0800

    save

--- data_downloader.py
+++ data_downloader.py
@@ -42,6 +42,13 @@ mysql_args = {
     "charset": "utf8"
 }
 
+headers = {'Accept': '*/*',
+            'Accept-Language': 'en-US,en;q=0.8',
+            'Cache-Control': 'max-age=0',
+            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.116 Safari/537.36',
+            'Connection': 'keep-alive'
+}
+
 class data_downloader(object):
 
     def __init__(self):
